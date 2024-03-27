d0aae73fafa6224ca9c12bf2720d68956e0f21d6
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-03-27 20:02:20 +0000

    adição de comentários ao dia 3 e 4

--- Cpp/Aulas/Aula_3/Aula_3b/aula_3b.cpp
+++ Cpp/Aulas/Aula_3/Aula_3b/aula_3b.cpp
@@ -8,17 +8,17 @@ int main()
     cin >> num;
     cout << "Este foi o valor que escolheste: " << num << endl;
     cout << "Vamos chama-lo de: num" << endl;
-    num += 3;
+    num += 3; // O += é uma forma somar e atribuir de forma mais abreviada.
     cout << "Agora num + 3 = " << num << endl;
-    num -= 2;
+    num -= 2; // O -= é semelhante mas para subtrair.
     cout << "Agora num - 2 = " << num << endl;
-    num *= 10;
+    num *= 10; // O *= é semelhante mas para multiplicar.
     cout << "Agora num * 10 = " << num << endl;
-    num /= 5;
+    num /= 5; // O /= é semelhante mas para dividir.
     cout << "Agora num / 5 = " << num << endl;
-    num++;
+    num++; // O ++ é para adicionar 1 à variavel.
     cout << "Agora num++ (incremento) equivale a: " << num << endl;
-    num--;
+    num--; // O -- é para subtrair 1 à variavel.
     cout << "Agora num-- (decremento) equivale a: " << num << endl;
     return 0;
 }
--- Cpp/Aulas/Aula_4/aula_4.cpp
+++ Cpp/Aulas/Aula_4/aula_4.cpp
@@ -6,10 +6,10 @@ int main()
     float num;
     cout << "Diga um valor: ";
     cin >> num;
-    if (num >= 100)
+    if (num >= 100) // O if como o nome diz "se" é uma condição e serve para se for verdade faz esta coisa.
     {
         cout << "O valor e maior do que 100.";
-    } else 
+    } else  // O else como o nome diz "se não" faz se for falso.
     {
         cout << "O valor nao e maior do que 100.";
     }
